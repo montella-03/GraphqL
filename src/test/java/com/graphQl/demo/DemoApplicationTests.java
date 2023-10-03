9a342460477dfd00f2b67c6b010f3baecf32fa00
CHRISTOPHER OIGO <oigoc60@gmail.com>
2023-10-03 13:12:41 +0300

    test

--- src/test/java/com/graphQl/demo/DemoApplicationTests.java
+++ src/test/java/com/graphQl/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.graphQl.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void test() {
+		assertEquals(1.0, 1.0);
+		
 	}
 
 }
